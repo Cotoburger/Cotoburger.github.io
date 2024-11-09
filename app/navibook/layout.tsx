6131ed97e6ff50ff6df89f60b6ab2e3af29c9605
ArtyomKor <artyom@artyomk.ru>
2024-11-09 20:55:08 +0700

    Scale fix

---
+++ app/navibook/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import React from "react";
+import Header from "@/app/navibook/header";
+
+export const metadata: Metadata = {
+  title: "NaviBook",
+  description: "The most advanced mod-free laptop for Scrap Mechanic.",
+  openGraph: {
+    title: "NaviBook",
+    description: "The most advanced mod-free laptop for Scrap Mechanic.",
+    images: ["https://cotoburger.github.io/mainpic.png"]
+  },
+  twitter: {
+    title: "NaviBook",
+    description: "The most advanced mod-free laptop for Scrap Mechanic.",
+    images: ["https://cotoburger.github.io/navibook.png"]
+  },
+
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ru">
+    <body
+        className={`antialiased`}
+    >
+    <Header/>
+
+    {children}
+    </body>
+    </html>
+  );
+}
