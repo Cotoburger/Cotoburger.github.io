7e6e5d5086569d73c78718a1235ea0c0d96c82f1
Catsburger <100293635+Cotoburger@users.noreply.github.com>
2024-11-10 22:35:34 +1200

    123123

--- Catsburger/script.js
+++ Catsburger/script.js
@@ -1,11 +1,11 @@
 // Инициализация Swiper
 const swiper = new Swiper('.swiper-container', {
     // Настройка пространства между слайдами
-    spaceBetween: 10,  // Расстояние между слайдами
+    spaceBetween: 20,  // Расстояние между слайдами
     slidesPerView: 'auto',  // Автоматическое количество слайдов, зависит от ширины
     loop: true,  // Зацикливаем слайды
     autoplay: {
-        delay: 3500,  // Интервал между прокрутками (в миллисекундах)
+        delay: 2500,  // Интервал между прокрутками (в миллисекундах)
         disableOnInteraction: false,  // Отключить автоматическую прокрутку, если пользователь взаимодействует
     },
     navigation: {
@@ -18,3 +18,35 @@ const swiper = new Swiper('.swiper-container', {
     },
     
 });
+
+// Находим все изображения на странице и аватарку
+const images = document.querySelectorAll('img');
+const avatar = document.querySelector('.avatar');
+
+// Отслеживание события прокрутки
+window.addEventListener('scroll', () => {
+    if (scrollY > 400) {
+        // Меняем фон на белый и делаем изображения прозрачными
+        document.body.style.backgroundColor = '#ffffff';
+        document.body.style.color = '#000000';
+
+        images.forEach((img) => {
+            // Пропускаем аватарку, изменяем прозрачность только других изображений
+            if (img !== avatar) {
+                img.style.opacity = '0';
+            }
+        });
+    } else {
+        // Восстанавливаем исходный фон и изображения
+        document.body.style.backgroundColor = '#000000';
+        document.body.style.color = '#b9b4b4';
+
+        images.forEach((img) => {
+            if (img !== avatar) {
+                img.style.opacity = '1';
+            }
+        });
+    }
+});
+
+
