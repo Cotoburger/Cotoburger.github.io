cefbe062cc9be47596cfd5df20b41f6e0ba50417
Catsburger <100293635+Cotoburger@users.noreply.github.com>
2024-11-15 12:26:35 +1200

    12

--- Catsburger/script.js
+++ Catsburger/script.js
@@ -228,108 +228,108 @@ document.addEventListener("DOMContentLoaded", () => {
 const schedule = {
     1: {
         shift1: [
-            {lesson: "Классный час", start: "08:15", end: "08:45"},
-            {lesson: "1 урок", start: "08:55", end: "09:35"},
-            {lesson: "2 урок", start: "09:50", end: "10:30"},
-            {lesson: "3 урок", start: "10:45", end: "11:25"},
-            {lesson: "4 урок", start: "11:30", end: "12:10"},
-            {lesson: "5 урок", start: "12:20", end: "13:00"},
-            {lesson: "6 урок", start: "13:20", end: "14:00"},
-            {lesson: "7 урок", start: "14:15", end: "14:55"}
+            {lesson: "Class Hour", start: "08:15", end: "08:45"},
+            {lesson: "1st Lesson", start: "08:55", end: "09:35"},
+            {lesson: "2nd Lesson", start: "09:50", end: "10:30"},
+            {lesson: "3rd Lesson", start: "10:45", end: "11:25"},
+            {lesson: "4th Lesson", start: "11:30", end: "12:10"},
+            {lesson: "5th Lesson", start: "12:20", end: "13:00"},
+            {lesson: "6th Lesson", start: "13:20", end: "14:00"},
+            {lesson: "7th Lesson", start: "14:15", end: "14:55"}
         ],
         shift2: [
-            {lesson: "0 урок", start: "12:20", end: "13:00"},
-            {lesson: "1 урок", start: "13:20", end: "14:00"},
-            {lesson: "2 урок", start: "14:15", end: "14:55"},
-            {lesson: "3 урок", start: "15:10", end: "15:50"},
-            {lesson: "Классный час", start: "16:05", end: "16:35"},
-            {lesson: "4 урок", start: "16:40", end: "17:20"},
-            {lesson: "5 урок", start: "17:30", end: "18:10"},
-            {lesson: "6 урок", start: "18:20", end: "19:00"}
+            {lesson: "0th Lesson", start: "12:20", end: "13:00"},
+            {lesson: "1st Lesson", start: "13:20", end: "14:00"},
+            {lesson: "2nd Lesson", start: "14:15", end: "14:55"},
+            {lesson: "3rd Lesson", start: "15:10", end: "15:50"},
+            {lesson: "Class Hour", start: "16:05", end: "16:35"},
+            {lesson: "4th Lesson", start: "16:40", end: "17:20"},
+            {lesson: "5th Lesson", start: "17:30", end: "18:10"},
+            {lesson: "6th Lesson", start: "18:20", end: "19:00"}
         ]
     },
     2: {
         shift1: [
-            {lesson: "1 урок", start: "08:15", end: "08:55"},
-            {lesson: "2 урок", start: "09:05", end: "09:45"},
-            {lesson: "3 урок", start: "10:00", end: "10:40"},
-            {lesson: "4 урок", start: "11:00", end: "11:40"},
-            {lesson: "5 урок", start: "11:50", end: "12:30"},
-            {lesson: "6 урок", start: "12:50", end: "13:30"},
-            {lesson: "7 урок", start: "13:50", end: "14:30"}
+            {lesson: "1st Lesson", start: "08:15", end: "08:55"},
+            {lesson: "2nd Lesson", start: "09:05", end: "09:45"},
+            {lesson: "3rd Lesson", start: "10:00", end: "10:40"},
+            {lesson: "4th Lesson", start: "11:00", end: "11:40"},
+            {lesson: "5th Lesson", start: "11:50", end: "12:30"},
+            {lesson: "6th Lesson", start: "12:50", end: "13:30"},
+            {lesson: "7th Lesson", start: "13:50", end: "14:30"}
         ],
         shift2: [
-            {lesson: "0 урок", start: "11:50", end: "12:30"},
-            {lesson: "1 урок", start: "12:50", end: "13:30"},
-            {lesson: "2 урок", start: "13:50", end: "14:30"},
-            {lesson: "3 урок", start: "14:45", end: "15:25"},
-            {lesson: "4 урок", start: "15:40", end: "16:20"},
-            {lesson: "5 урок", start: "16:30", end: "17:10"},
-            {lesson: "6 урок", start: "17:20", end: "18:00"},
+            {lesson: "0th Lesson", start: "11:50", end: "12:30"},
+            {lesson: "1st Lesson", start: "12:50", end: "13:30"},
+            {lesson: "2nd Lesson", start: "13:50", end: "14:30"},
+            {lesson: "3rd Lesson", start: "14:45", end: "15:25"},
+            {lesson: "4th Lesson", start: "15:40", end: "16:20"},
+            {lesson: "5th Lesson", start: "16:30", end: "17:10"},
+            {lesson: "6th Lesson", start: "17:20", end: "18:00"}
         ]
     },
     3: {
         shift1: [
-            {lesson: "1 урок", start: "08:15", end: "08:55"},
-            {lesson: "2 урок", start: "09:05", end: "09:45"},
-            {lesson: "3 урок", start: "10:00", end: "10:40"},
-            {lesson: "Классный час", start: "10:55", end: "11:25"},
-            {lesson: "4 урок", start: "11:30", end: "12:10"},
-            {lesson: "5 урок", start: "12:20", end: "13:00"},
-            {lesson: "6 урок", start: "13:20", end: "14:00"},
-            {lesson: "7 урок", start: "14:15", end: "14:55"}
+            {lesson: "1st Lesson", start: "08:15", end: "08:55"},
+            {lesson: "2nd Lesson", start: "09:05", end: "09:45"},
+            {lesson: "3rd Lesson", start: "10:00", end: "10:40"},
+            {lesson: "Class Hour", start: "10:55", end: "11:25"},
+            {lesson: "4th Lesson", start: "11:30", end: "12:10"},
+            {lesson: "5th Lesson", start: "12:20", end: "13:00"},
+            {lesson: "6th Lesson", start: "13:20", end: "14:00"},
+            {lesson: "7th Lesson", start: "14:15", end: "14:55"}
         ],
         shift2: [
-            {lesson: "0 урок", start: "12:20", end: "13:00"},
-            {lesson: "1 урок", start: "13:20", end: "14:00"},
-            {lesson: "2 урок", start: "14:15", end: "14:55"},
-            {lesson: "3 урок", start: "15:10", end: "15:50"},
-            {lesson: "Классный час", start: "16:05", end: "16:35"},
-            {lesson: "4 урок", start: "16:40", end: "17:20"},
-            {lesson: "5 урок", start: "17:30", end: "18:10"},
-            {lesson: "6 урок", start: "18:20", end: "19:00"}
+            {lesson: "0th Lesson", start: "12:20", end: "13:00"},
+            {lesson: "1st Lesson", start: "13:20", end: "14:00"},
+            {lesson: "2nd Lesson", start: "14:15", end: "14:55"},
+            {lesson: "3rd Lesson", start: "15:10", end: "15:50"},
+            {lesson: "Class Hour", start: "16:05", end: "16:35"},
+            {lesson: "4th Lesson", start: "16:40", end: "17:20"},
+            {lesson: "5th Lesson", start: "17:30", end: "18:10"},
+            {lesson: "6th Lesson", start: "18:20", end: "19:00"}
         ]
     },
     4: {
         shift1: [
-            {lesson: "1 урок", start: "08:15", end: "08:55"},
-            {lesson: "2 урок", start: "09:05", end: "09:45"},
-            {lesson: "3 урок", start: "10:00", end: "10:40"},
-            {lesson: "Классный час", start: "10:55", end: "11:25"},
-            {lesson: "4 урок", start: "11:30", end: "12:10"},
-            {lesson: "5 урок", start: "12:20", end: "13:00"},
-            {lesson: "6 урок", start: "13:20", end: "14:00"},
-            {lesson: "7 урок", start: "14:15", end: "14:55"}
+            {lesson: "1st Lesson", start: "08:15", end: "08:55"},
+            {lesson: "2nd Lesson", start: "09:05", end: "09:45"},
+            {lesson: "3rd Lesson", start: "10:00", end: "10:40"},
+            {lesson: "Class Hour", start: "10:55", end: "11:25"},
+            {lesson: "4th Lesson", start: "11:30", end: "12:10"},
+            {lesson: "5th Lesson", start: "12:20", end: "13:00"},
+            {lesson: "6th Lesson", start: "13:20", end: "14:00"},
+            {lesson: "7th Lesson", start: "14:15", end: "14:55"}
         ],
         shift2: [
-            {lesson: "0 урок", start: "12:20", end: "13:00"},
-            {lesson: "1 урок", start: "13:20", end: "14:00"},
-            {lesson: "2 урок", start: "14:15", end: "14:55"},
-            {lesson: "3 урок", start: "15:10", end: "15:50"},
-            {lesson: "Классный час", start: "16:05", end: "16:35"},
-            {lesson: "4 урок", start: "16:40", end: "17:20"},
-            {lesson: "5 урок", start: "17:30", end: "18:10"},
-            {lesson: "6 урок", start: "18:20", end: "19:00"}
+            {lesson: "0th Lesson", start: "12:20", end: "13:00"},
+            {lesson: "1st Lesson", start: "13:20", end: "14:00"},
+            {lesson: "2nd Lesson", start: "14:15", end: "14:55"},
+            {lesson: "3rd Lesson", start: "15:10", end: "15:50"},
+            {lesson: "Class Hour", start: "16:05", end: "16:35"},
+            {lesson: "4th Lesson", start: "16:40", end: "17:20"},
+            {lesson: "5th Lesson", start: "17:30", end: "18:10"},
+            {lesson: "6th Lesson", start: "18:20", end: "19:00"}
         ]
     },
     5: {
         shift1: [
-            {lesson: "1 урок", start: "08:15", end: "08:55"},
-            {lesson: "2 урок", start: "09:05", end: "09:45"},
-            {lesson: "3 урок", start: "10:00", end: "10:40"},
-            {lesson: "4 урок", start: "11:00", end: "11:40"},
-            {lesson: "5 урок", start: "11:50", end: "12:30"},
-            {lesson: "6 урок", start: "12:50", end: "13:30"},
-            {lesson: "7 урок", start: "13:50", end: "14:30"}
+            {lesson: "1st Lesson", start: "08:15", end: "08:55"},
+            {lesson: "2nd Lesson", start: "09:05", end: "09:45"},
+            {lesson: "3rd Lesson", start: "10:00", end: "10:40"},
+            {lesson: "4th Lesson", start: "11:00", end: "11:40"},
+            {lesson: "5th Lesson", start: "11:50", end: "12:30"},
+            {lesson: "6th Lesson", start: "12:50", end: "13:30"},
+            {lesson: "7th Lesson", start: "13:50", end: "14:30"}
         ],
         shift2: [
-            {lesson: "0 урок", start: "11:50", end: "12:30"},
-            {lesson: "1 урок", start: "12:50", end: "13:30"},
-            {lesson: "2 урок", start: "13:50", end: "14:30"},
-            {lesson: "3 урок", start: "14:45", end: "15:25"},
-            {lesson: "4 урок", start: "15:40", end: "16:20"},
-            {lesson: "5 урок", start: "16:30", end: "17:10"},
-            {lesson: "6 урок", start: "17:20", end: "18:00"},
+            {lesson: "0th Lesson", start: "11:50", end: "12:30"},
+            {lesson: "1st Lesson", start: "12:50", end: "13:30"},
+            {lesson: "2nd Lesson", start: "13:50", end: "14:30"},
+            {lesson: "3rd Lesson", start: "14:45", end: "15:25"},
+            {lesson: "4th Lesson", start: "15:40", end: "16:20"},
+            {lesson: "5th Lesson", start: "16:30", end: "17:10"},
+            {lesson: "6th Lesson", start: "17:20", end: "18:00"}
         ]
     }
 };
