48d8cce73f361b07414506ca1ecb5e78803ee648
Catsburger <100293635+Cotoburger@users.noreply.github.com>
2024-12-18 23:50:41 +1200

    Update script2.js

--- School1/script2.js
+++ School1/script2.js
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
     const snowflakesContainer = document.getElementById("snowflakes");
-    const maxSnowflakes = 40; // Максимальное количество снежинок на экране
+    const maxSnowflakes = 30; // Максимальное количество снежинок на экране
 
     function createSnowflake() {
         // Если количество снежинок на экране больше лимита, не создаем новые
@@ -94,7 +94,7 @@ document.addEventListener("DOMContentLoaded", () => {
         createSnowflake();
         setTimeout(() => {
             requestAnimationFrame(snowflakesLoop);
-        }, 200); // Увеличен интервал между созданием снежинок
+        }, 275); // Увеличен интервал между созданием снежинок
     }
 
     // Запуск анимации снежинок
