e58212c1bbac3550025a4aa5afea931cc9879365
Catsburger <100293635+Cotoburger@users.noreply.github.com>
2024-12-29 03:28:31 +1200

    cache fix

--- School1/sw.js
+++ School1/sw.js
@@ -1,69 +1,62 @@
-const cacheName = 'Cache-v1'; // Название текущего кэша
+const cacheName = 'Cache';
 const filesToCache = [
     'DebugInfo.txt',
-    'arabic.html',
-    'about.html',
-    'index.html',
-    'script.js',
-    'script2.js',
-    'images/icon.png',
-    'snowflake.svg',
-    'styles.css',
-    'images/image-26.png',
-    'aos.js',
-    'aos.css',
-    'images/sun.svg',
-    'images/moon.svg',
+    "arabic.html",
+    "about.html",
+    "index.html",
+    "script.js",
+    "script2.js",
+    "images/icon.png",
+    "snowflake.svg",
+    "styles.css",
+    "images/image-26.png",
+    "aos.js",
+    "aos.css",
+    "images/sun.svg",
+    "images/moon.svg",
 ];
 
-// Событие установки Service Worker
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(cacheName)
-            .then((cache) => {
-                console.log('Caching files...');
-                return cache.addAll(filesToCache);
-            })
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
     );
+    self.skipWaiting();
 });
 
-// Событие активации Service Worker
 self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [cacheName];
-
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
-                cacheNames.map((cache) => {
-                    if (!cacheWhitelist.includes(cache)) {
-                        console.log('Deleting old cache:', cache);
-                        return caches.delete(cache); // Удаляем старые кэши
+                cacheNames.map((oldCacheName) => {
+                    if (oldCacheName !== cacheName) {
+                        return caches.delete(oldCacheName);
                     }
                 })
             );
         })
     );
+    self.clients.claim();
 });
 
-// Событие запроса ресурсов (fetch)
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then((cachedResponse) => {
-                if (cachedResponse) {
-                    return cachedResponse; // Возвращаем ресурс из кэша
-                }
+        caches.match(event.request).then((cachedResponse) => {
+            const fetchPromise = fetch(event.request).then((networkResponse) => {
 
-                // Если ресурса нет в кэше, запрашиваем его из сети
-                return fetch(event.request).then((networkResponse) => {
-                    // Кэшируем ответ на новый запрос, если нужно
-                    if (event.request.url.startsWith(self.location.origin)) {
-                        caches.open(cacheName).then((cache) => {
-                            cache.put(event.request, networkResponse.clone());
-                        });
-                    }
-                    return networkResponse;
-                });
-            })
+                if (networkResponse && networkResponse.ok) {
+                    const clonedResponse = networkResponse.clone();
+                    caches.open(cacheName).then((cache) => {
+                        cache.put(event.request, clonedResponse);
+                    });
+                }
+                return networkResponse;
+            }).catch((err) => {
+                console.error('Ошибка при запросе:', err);
+            });
+
+            return cachedResponse || fetchPromise;
+        })
     );
 });
