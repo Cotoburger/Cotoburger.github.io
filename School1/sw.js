0da819c38946353c9a3e62f552c535e0dbaeef03
Catsburger <100293635+Cotoburger@users.noreply.github.com>
2025-01-25 19:31:16 +1200

    fix update

--- School1/sw.js
+++ School1/sw.js
@@ -1,58 +1,59 @@
-
-const CACHE_NAME = 'school-app-v1.12';
-const STATIC_ASSETS = [
-  '/index.html',
-  '/script.js',
-  '/script2.js',
-  '/aos.js',
-  '/styles.css',
-  '/images/image-26.png'
+const cacheName = 'Cache';
+const filesToCache = [
+    'DebugInfo.txt',
+    "arabic.html",
+    "about.html",
+    "index.html",
+    "script.js",
+    "script2.js",
+    "images/icon.png",
+    "snowflake.svg",
+    "styles.css",
+    "images/image-26.png",
+    "aos.js",
+    "aos.css",
+    "images/sun.svg",
+    "images/moon.svg",
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        // Удаляем все старые файлы, чтобы пересоздать кэш
-        return cache.addAll(STATIC_ASSETS);
-      })
-  );
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
+    );
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((oldCacheName) => {
+                    if (oldCacheName !== cacheName) {
+                        return caches.delete(oldCacheName);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(
-          (response) => {
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
+    event.respondWith(
+        fetch(event.request).then((networkResponse) => {
+            // Проверяем, что запрос успешен
+            if (networkResponse && networkResponse.ok) {
+                const clonedResponse = networkResponse.clone();
+                caches.open(cacheName).then((cache) => {
+                    cache.put(event.request, clonedResponse);  // Обновляем кэш
+                });
             }
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
-      })
-  );
+            return networkResponse;
+        }).catch(() => {
+            // Если сеть недоступна, возвращаем кэшированные данные
+            return caches.match(event.request);
+        })
+    );
 });
