4e5c7af15f10bd771a89665be9fdc1b5ba87488f
Catsburger <100293635+Cotoburger@users.noreply.github.com>
2025-01-28 19:16:40 +1200

    Update accelerometer.js

--- School1/accelerometer.js
+++ School1/accelerometer.js
@@ -14,7 +14,7 @@ function handleDeviceMotion(event) {
     const acceleration = event.accelerationIncludingGravity || { x: 0, y: 0, z: 0 };
     const currentTime = new Date().getTime();
 
-    if ((currentTime - lastUpdate) > 300) {
+    if ((currentTime - lastUpdate) > 250) {
         const timeDifference = (currentTime - lastUpdate) / 1000;
         lastUpdate = currentTime;
 
