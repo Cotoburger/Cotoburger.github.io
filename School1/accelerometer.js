75415b15a315b5fd5110fe422eec1ad82f33edd9
Catsburger <100293635+Cotoburger@users.noreply.github.com>
2025-01-28 23:51:41 +1200

    Update accelerometer.js

--- School1/accelerometer.js
+++ School1/accelerometer.js
@@ -84,10 +84,14 @@ function monitorPopupMotion() {
         const acceleration = event.accelerationIncludingGravity || { x: 0, y: 0, z: 0 };
         const z = acceleration.z;
 
+        console.log('Z-axis acceleration:', z); // Логируем значение оси Z
+
         // Проверяем направление движения
-        if (z > 100) { // Телефон поднимается вверх
+        if (z > 5) { // Телефон поднимается вверх
+            console.log('Phone moved up');
             window.location.href = 'https://example.com/page-up';
-        } else if (z < -100) { // Телефон опускается вниз
+        } else if (z < -5) { // Телефон опускается вниз
+            console.log('Phone moved down');
             window.location.href = 'https://example.com/page-down';
         }
     };
@@ -97,6 +101,7 @@ function monitorPopupMotion() {
     // Убираем слушатель после закрытия попапа
     setTimeout(() => {
         window.removeEventListener('devicemotion', motionListener);
+        console.log('Motion listener removed');
     }, 5000); // Длительность проверки совпадает с показом попапа
 }
 
